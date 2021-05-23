ca38a8aa8fc9c0bd40b730df10d6337920ae1c6a
SkullAG <>
2021-05-23 18:49:31 +0000

    databaseeee

--- web/php/newuser.php
+++ web/php/newuser.php
@@ -2,10 +2,10 @@
 
 	$numero = $_REQUEST["numero"];
 
-	//$db = parse_url(getenv("DATABASE_URL"));
-	//$db["path"] = ltrim($db["path"], "/");
+	$db = parse_url(getenv("postgres://xwtkuhegmhwgib:5045da018ef45f5de1ff540679eee65915e03d1bf4b4e0d61a52d7c23d0f0efe@ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/d2dhrlcmfkusbr"));
+	$db["d2dhrlcmfkusbr"] = ltrim($db["d2dhrlcmfkusbr"], "/");
 
-	$db = $numero;
+	//$db = $numero;
 
 	echo $db;
 ?>
