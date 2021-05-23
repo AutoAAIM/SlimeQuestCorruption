2eeb9305013bf5c64119034a8305409e7d9a12be
SkullAG <>
2021-05-23 18:35:36 +0000

    papo

--- web/php/newuser.php
+++ web/php/newuser.php
@@ -2,10 +2,10 @@
 
 	$numero = $_REQUEST("numero");
 
-	$db = parse_url(getenv("DATABASE_URL"));
-	$db["path"] = ltrim($db["path"], "/");
+	//$db = parse_url(getenv("DATABASE_URL"));
+	//$db["path"] = ltrim($db["path"], "/");
 
-	//$db = $numero * 3;
+	$db = $numero;
 
 	echo $db;
 ?>
