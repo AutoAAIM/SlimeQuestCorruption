64c26481a673cab40a34a3a70a0982f7dc36146f
SkullAG <>
2021-05-23 23:30:35 +0000

    db

--- web/php/newuser.php
+++ web/php/newuser.php
@@ -21,9 +21,9 @@
 		}
 	}
 
-	//$rstr = implode("?", $respuesta);
+	$rstr = implode("?", $respuesta);
 	//$rstr = print_r($respuesta, true);
-	$rstr = implode("?",$respuesta));
+	//$rstr = implode("?",$respuesta));
 	//print_r($out);
 	//$rstr = json_encode($respuesta);
 	//$rstr = convert_multi_array($respuesta);
