a8e203dc3567440476f55d484fddcb9b107698ca
SkullAG <>
2021-05-25 14:26:01 +0000

    db

--- web/php/loguser.php
+++ web/php/loguser.php
@@ -1,10 +1,13 @@
 <?php
-	require 'SQLGlobal.php';
+	require_once 'db_config.php';
+
+	$db = pg_connect("host=ec2-54-72-155-238.eu-west-1.compute.amazonaws.com port=5432 dbname=d2dhrlcmfkusbr user=$user password=5045da018ef45f5de1ff540679eee65915e03d1bf4b4e0d61a52d7c23d0f0efe");
 
 	if($_SERVER['REQUEST_METHOD']=='GET'){
 		try{
-			$respuesta = SQLGlobal::selectArray("select * from usuarios");
-			$respuestaz = SQLGlobal::selectArray("select * from zonas");
+			$respuesta = pg_query("select * from usuarios");
+			$respuestaz = pg_query("select * from zonas");
+
 			echo json_encode(array(
 				'respuesta'=>'200',
 				'estado' => 'Se obtuvieron los datos correctamente',
@@ -25,12 +28,8 @@
 	}
 
 	$rstr = json_encode($respuesta, true);
-	//$rstr = implode(",", (array)$respuesta);
-	//$rstr = implode($rstr);
 	echo $rstr;
 	echo '?';
 	echo json_encode($respuestaz, true);
-	//echo var_dump($respuesta);
-	//echo var_dump($rstr);
-
+	
 ?>
