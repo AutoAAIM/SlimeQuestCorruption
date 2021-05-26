3b234acdc25a466107eff025e41a44ea3a88eae2
SkullAG <>
2021-05-26 18:30:50 +0000

    lago

--- web/js/mapas/snowyMountain.js
+++ web/js/mapas/snowyMountain.js
@@ -5,29 +5,6 @@ import  * as oscuridad from './mapa.js';
 import  * as portal from './tp.js';
 import  * as bossHielo from './bossHielo.js';
 
-var config = {
-	type: Phaser.AUTO,
-	width:600,
-	height:400,
-	pixelArt:true,
-	physics:{
-		default:'arcade',
-		arcade:{
-			debug:false,
-			gravity:{y:0}
-		}
-	},
-	scene:{
-		preload:preload,
-		create:create,
-		update:update
-	},
-	scale:{
-		zoom:2
-	}
-}
-
-var game = new Phaser.Game(config);
 var scene;
 var contFuego;
 var contHielo;
@@ -64,121 +41,127 @@ var Hielo;
 var i;
 var f;
 
-export function preload()
-{
-	//this.load.atlas('atlas', 'assets/atlas/atlas.png', 'assets/atlas/sprites.json');
-	this.load.image('cuevaTiles', 'assets/mapas/terrain.png');
-	this.load.image('snowTiles', 'assets/mapas/snow.png');
-	this.load.tilemapTiledJSON('CuevaMago', 'assets/mapas/mapa_CuevaMago.json');
+export default class lago extends Phaser.Scene {
+	constructor(){
+		super("lago");
+	}
 
-	yasha.preload.call(this)
-	jotun.preload.call(this)
-	mago.preload.call(this)
-	portal.preload.call(this)
-	bossHielo.preload.call(this)
-}
+	preload()
+	{
+		//this.load.atlas('atlas', 'assets/atlas/atlas.png', 'assets/atlas/sprites.json');
+		this.load.image('cuevaTiles', 'assets/mapas/terrain.png');
+		this.load.image('snowTiles', 'assets/mapas/snow.png');
+		this.load.tilemapTiledJSON('CuevaMago', 'assets/mapas/mapa_CuevaMago.json');
 
-function create()
-{
-	scene = this;
+		yasha.preload.call(this)
+		jotun.preload.call(this)
+		mago.preload.call(this)
+		portal.preload.call(this)
+		bossHielo.preload.call(this)
+	}
 
-	const mapa = this.make.tilemap({key:'CuevaMago'});
+	create()
+	{
+		scene = this;
 
-	const tileset = mapa.addTilesetImage('terrain', 'cuevaTiles');
-	const tileset2 = mapa.addTilesetImage('snow', 'snowTiles');
+		const mapa = this.make.tilemap({key:'CuevaMago'});
 
-	allTilesets = [tileset, tileset2]
+		const tileset = mapa.addTilesetImage('terrain', 'cuevaTiles');
+		const tileset2 = mapa.addTilesetImage('snow', 'snowTiles');
 
-	lago = mapa.createLayer('lago', allTilesets).setDepth(2).setPipeline('Light2D');
-	fondo = mapa.createLayer('fondo', allTilesets).setPipeline('Light2D');
-	tierra = mapa.createLayer('tierra', allTilesets).setPipeline('Light2D');
-	luz = mapa.createLayer('luz', allTilesets).setPipeline('Light2D');
-	muros = mapa.createLayer('muros', allTilesets).setDepth(0).setPipeline('Light2D');
-	objetos = mapa.createLayer('objetos', allTilesets).setDepth(4).setPipeline('Light2D');
-	objetos2 = mapa.createLayer('objetos2', allTilesets).setDepth(1).setPipeline('Light2D');
+		allTilesets = [tileset, tileset2]
 
-	mapa.x = 0;
-	mapa.y = 0;
+		lago = mapa.createLayer('lago', allTilesets).setDepth(2).setPipeline('Light2D');
+		fondo = mapa.createLayer('fondo', allTilesets).setPipeline('Light2D');
+		tierra = mapa.createLayer('tierra', allTilesets).setPipeline('Light2D');
+		luz = mapa.createLayer('luz', allTilesets).setPipeline('Light2D');
+		muros = mapa.createLayer('muros', allTilesets).setDepth(0).setPipeline('Light2D');
+		objetos = mapa.createLayer('objetos', allTilesets).setDepth(4).setPipeline('Light2D');
+		objetos2 = mapa.createLayer('objetos2', allTilesets).setDepth(1).setPipeline('Light2D');
 
-	allTiles = [objetos, muros, fondo, objetos2, lago, luz, tierra];
+		mapa.x = 0;
+		mapa.y = 0;
 
-	tileFuego = mapa.createFromObjects('fuegoTiles');
-	antorchas = new Array();
-	tileFuego.forEach(obj => {
-		this.physics.world.enable(obj);
-		obj.setAlpha(0);
-		obj.encendido = false;
+		allTiles = [objetos, muros, fondo, objetos2, lago, luz, tierra];
 
-		antorchas.unshift(obj);
-	});
+		tileFuego = mapa.createFromObjects('fuegoTiles');
+		antorchas = new Array();
+		tileFuego.forEach(obj => {
+			this.physics.world.enable(obj);
+			obj.setAlpha(0);
+			obj.encendido = false;
 
-	tilePortal = mapa.createFromObjects('tpTiles');
-	gates = portal.create(tilePortal);
+			antorchas.unshift(obj);
+		});
 
-	jotun.create();
+		tilePortal = mapa.createFromObjects('tpTiles');
+		gates = portal.create(tilePortal);
 
-	yasha.create(allTiles, antorchas, config);
-	jotun.create();
-	oscuridad.create(scene, allTiles);
+		jotun.create();
 
-	tilePortal.forEach(obj => {
+		yasha.create(allTiles, antorchas, config);
+		jotun.create();
+		oscuridad.create(scene, allTiles);
 
-		if(obj.name == 'mago')
-		{
-			obj.setAlpha(0)
-			mago.create(obj);
-		}
+		tilePortal.forEach(obj => {
 
-		if(obj.name == 'jotun')
-		{
-			obj.setAlpha(0)
-			jotun.generarEnemigo(obj);
-		}
+			if(obj.name == 'mago')
+			{
+				obj.setAlpha(0)
+				mago.create(obj);
+			}
 
-		if(obj.name == 'boss')
-		{
-			obj.setAlpha(0)
-			bossHielo.create(obj);
-		}
-	});
+			if(obj.name == 'jotun')
+			{
+				obj.setAlpha(0)
+				jotun.generarEnemigo(obj);
+			}
 
-	freezeTiles = lago.filterTiles(tile => tile.properties.ice).map(x => x.index);
-	darkTiles = luz.filterTiles(tile => tile.properties.dark).map(x => x.index);
-	snowTiles = objetos.filterTiles(tile => tile.properties.snow).map(x => x.index);
+			if(obj.name == 'boss')
+			{
+				obj.setAlpha(0)
+				bossHielo.create(obj);
+			}
+		});
 
-	yasha.setFreeze(lago, freezeTiles);
-	var cosas = yasha.grupoHielo
+		freezeTiles = lago.filterTiles(tile => tile.properties.ice).map(x => x.index);
+		darkTiles = luz.filterTiles(tile => tile.properties.dark).map(x => x.index);
+		snowTiles = objetos.filterTiles(tile => tile.properties.snow).map(x => x.index);
 
- 	lago.setTileIndexCallback(freezeTiles, yasha.freeze, this.physics.add.overlap(yasha.grupoHielo, lago));
-	 
-	lago.setTileIndexCallback(freezeTiles, yasha.freeze, this.physics.add.overlap(yasha.grupoFuego, lago));
+		yasha.setFreeze(lago, freezeTiles);
+		var cosas = yasha.grupoHielo
 
-	luz.setTileIndexCallback(darkTiles, oscuridad.encenderOscuridad, this.physics.add.overlap(yasha, luz));
+		lago.setTileIndexCallback(freezeTiles, yasha.freeze, this.physics.add.overlap(yasha.grupoHielo, lago));
+		
+		lago.setTileIndexCallback(freezeTiles, yasha.freeze, this.physics.add.overlap(yasha.grupoFuego, lago));
 
-	objetos.setTileIndexCallback(snowTiles, yasha.derretir, this.physics.add.overlap(yasha.grupoFuego, objetos));
+		luz.setTileIndexCallback(darkTiles, oscuridad.encenderOscuridad, this.physics.add.overlap(yasha, luz));
+
+		objetos.setTileIndexCallback(snowTiles, yasha.derretir, this.physics.add.overlap(yasha.grupoFuego, objetos));
+		
+
+		this.physics.add.collider(mago.mago, yasha.player);
+		this.physics.add.collider(jotun.grupoEnemigos, yasha.player);
+		this.physics.add.collider(bossHielo.boss, yasha.player);
 	
+		objetos.setCollisionByProperty({collides: true});
+		muros.setCollisionByProperty({collides: true});
+		objetos2.setCollisionByProperty({collides: true});
 
-	this.physics.add.collider(mago.mago, yasha.player);
-	this.physics.add.collider(jotun.grupoEnemigos, yasha.player);
-	this.physics.add.collider(bossHielo.boss, yasha.player);
- 
-	objetos.setCollisionByProperty({collides: true});
-	muros.setCollisionByProperty({collides: true});
-	objetos2.setCollisionByProperty({collides: true});
+		portal.collisionPortal(yasha.player);
 
-	portal.collisionPortal(yasha.player);
+		this.physics.add.overlap(yasha.player, mago.mago.detectionbox, yasha.encenderHielito, null, this);
 
-	this.physics.add.overlap(yasha.player, mago.mago.detectionbox, yasha.encenderHielito, null, this);
+		this.physics.add.overlap(yasha.player, jotun.grupoDispEnemigo, jotun.quitarVida, null, this);
+	}
 
-	this.physics.add.overlap(yasha.player, jotun.grupoDispEnemigo, jotun.quitarVida, null, this);
-}
-
-function update()
-{
-	yasha.update();
-	portal.update();
-	oscuridad.darkMode();
-	mago.magoTrue(antorchas);
-	jotun.updateDispEnem();
-	bossHielo.updateNieblaBoss();
+	update()
+	{
+		yasha.update();
+		portal.update();
+		oscuridad.darkMode();
+		mago.magoTrue(antorchas);
+		jotun.updateDispEnem();
+		bossHielo.updateNieblaBoss();
+	}
 }
