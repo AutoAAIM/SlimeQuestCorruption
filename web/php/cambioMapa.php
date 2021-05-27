8c39dbc543d3aafd07e7571baf0694fa905758c8
SkullAG <>
2021-05-27 09:54:24 +0000

    a

--- web/php/cambioMapa.php
+++ web/php/cambioMapa.php
@@ -7,9 +7,6 @@
 	$zona = $_REQUEST["zona"];
 
 	$db = pg_connect("host=$dbserver port=5432 dbname=$database user=$user password=$password");
-	$update = "UPDATE usuarios SET id_zona=$zona, dinero=$dinero WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
-	//echo $query;
-	pg_query($update);
 
 	$queryz = "select usuarios.nombre, usuarios.contrasena, usuarios.dinero, zonas.nombre as zonanombre from usuarios, zonas where usuarios.nombre = '$nombre' and usuarios.contrasena = '$contrasena' and usuarios.id_zona = zonas.id_zona";
 
@@ -18,6 +15,6 @@
 
 	$result = pg_query($queryz);
 
-	echo json_encode(pg_fetch_all($respuesta, true));
+	echo json_encode(pg_fetch_all($respuesta, true));;
 
 ?>
