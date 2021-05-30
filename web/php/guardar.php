16f39910ef5a83b1a92f6e9dea9263b6b528d55c
SkullAG <>
2021-05-30 17:32:19 +0000

    apa

--- web/php/guardar.php
+++ web/php/guardar.php
@@ -11,7 +11,7 @@
 	$objeto4 = $_REQUEST["inventario4"];
 
 	$db = pg_connect("host=$dbserver port=5432 dbname=$database user=$user password=$password");
-	$update = "UPDATE usuarios SET id_zona=$zona, dinero=$dinero, inventario1=$objeto1, inventario2=$objeto2, inventario3=$objeto3, inventario4=$objeto4 WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
+	$update = "UPDATE usuarios SET id_zona=$zona, dinero=$dinero, inventario1='$objeto1', inventario2='$objeto2', inventario3='$objeto3', inventario4='$objeto4' WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
 	//echo $query;
 	pg_query($update);
 
