a0a2c1001b42c4c3a37017e0b7a1840665974e31
MariaLopezifp <>
2021-05-30 17:27:13 +0000

    pium

--- web/php/guardar.php
+++ web/php/guardar.php
@@ -5,9 +5,13 @@
 	$contrasena = $_REQUEST["contrasena"];
 	$dinero = $_REQUEST["dinero"];
 	$zona = $_REQUEST["zona"];
+	$objeto1 = $_REQUEST["inventario1"];
+	$objeto2 = $_REQUEST["inventario2"];
+	$objeto3 = $_REQUEST["inventario3"];
+	$objeto4 = $_REQUEST["inventario4"];
 
 	$db = pg_connect("host=$dbserver port=5432 dbname=$database user=$user password=$password");
-	$update = "UPDATE usuarios SET id_zona=$zona, dinero=$dinero WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
+	$update = "UPDATE usuarios SET id_zona=$zona, dinero=$dinero, inventario1=$objeto1, inventario2=$objeto2, inventario3=$objeto3, inventario4=$objeto4 WHERE nombre = '$nombre' AND contrasena = '$contrasena'";
 	//echo $query;
 	pg_query($update);
 
