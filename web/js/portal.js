70cd95e3713fd9594acbb1ff2da9f56c4aa92d8d
SkullAG <>
2021-05-30 17:13:00 +0000

    peregil

--- web/js/portal.js
+++ web/js/portal.js
@@ -184,20 +184,22 @@ function changeScene(obj)
 
 			}
 		}
+
+		var objetos = new Array;
 		
 		for(var i = 0; i < 4; i++)
         {
-            console.log("H"+inventario.grupoObjetos.getChildren()[i]);
-            if(inventario.grupoObjetos.getChildren()[i] == undefined){
-				inventario.grupoObjetos.getChildren()[i] = new Object;
-                inventario.grupoObjetos.getChildren()[i].name = "";
+            if(inventario.grupoObjetos.getChildren()[i] != undefined){
+               objetos[i] = inventario.grupoObjetos.getChildren()[i].name
             }
-            console.log("s"+inventario.grupoObjetos.getChildren()[i]);
+			else{
+				objetos[i] = null
+			}
         }
 		console.log(obj.properties.destino)
 		console.log(myObj.nombre)
 		xhr.open("POST", "php/guardar.php", true)
 		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded")
-		xhr.send("nombre="+myObj.nombre+"&contrasena="+myObj.contrasena+"&dinero="+myObj.dinero+"&zona="+obj.properties.destino/*+"&inventario1="+inventario.grupoObjetos.getChildren()[0].name+"&inventario2="+inventario.grupoObjetos.getChildren()[1].name+"&inventario3="+inventario.grupoObjetos.getChildren()[2].name+"&inventario4="+inventario.grupoObjetos.getChildren()[3].name*/)
+		xhr.send("nombre="+myObj.nombre+"&contrasena="+myObj.contrasena+"&dinero="+myObj.dinero+"&zona="+obj.properties.destino+"&inventario1="+objetos[0]+"&inventario2="+objetos[1]+"&inventario3="+objetos[2]+"&inventario4="+objetos[3])
 	}
 }
