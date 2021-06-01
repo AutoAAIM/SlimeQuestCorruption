b936d06303670160d7f5cc0e6ca5c32bdc167244
MariaLopezifp <>
2021-06-01 10:17:11 +0000

    pero si no he tocado nada

--- web/js/mapas/snowyMountain.js
+++ web/js/mapas/snowyMountain.js
@@ -208,8 +208,8 @@ export default class montelago extends Phaser.Scene {
 
 					if (value <= 0 && pj.heroe)
 					{
-						pj.x = spawn[spawnID].x;
-						pj.y = spawn[spawnID].y;
+						pj.x = 200;
+						pj.y = 200;
 						if (tween.getValue() <= 0)
 						{
 							pj.vida -= 2;
